9e717f0188067cca976c60188f4e6a21436f7e7c
Yao <cyaoen@gmail.com>
2025-08-06 23:59:03 +0800

    set default page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,11 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
     {
+        path: '/',
+        name: 'Home',
+        component: () => import("@/views/HomeView.vue")
+    },
+    {
         path: '/home',
         name: 'home',
         component: () => import("@/views/HomeView.vue")
